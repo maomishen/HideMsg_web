0ad87b705c091417d1bc0994dfd1894788f612ac
Luna Gao <luna.gao@synrevoice.com.cn>
2017-08-14 23:04:28 +0800

    init

---
+++ HideMessage/Controllers/DetailController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HideMessage.Controllers
+{
+    public class DetailController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+    }
+}
