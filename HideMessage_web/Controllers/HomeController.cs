ed17cf2cb8e1e56ba42ac5e8b5d83bc7eea94bdf
LunaGao <gao.xue.qi@qq.com>
2017-08-20 00:04:50 +0800

    add cookie, login logic, devices sgin page.

--- HideMessage_web/Controllers/HomeController.cs
+++ HideMessage_web/Controllers/HomeController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using HideMessage_web.Data;
+using HideMessage_web.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace HideMessage_web.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            ViewData["Message"] = "Your application description page.";
+            string userName = User.FindFirst(ClaimTypes.GivenName).Value;
+            ViewBag.Title = userName;
             return View();
         }
     }
--- HideMessage_web/Controllers/LoginController.cs
+++ HideMessage_web/Controllers/LoginController.cs
@@ -3,17 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using HideMessage_web.Data;
+using HideMessage_web.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace HideMessage_web.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
-        // GET: /<controller>/
+        [HttpGet]
         public IActionResult Index()
         {
+            ViewBag.Title = "登录";
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(User userFromFore)
+        {
+            using (var context = new DataContext())
+            {
+                var userFromDB = context.Users.FirstOrDefault(u => u.user_account == userFromFore.user_account
+                                                            && u.user_password == userFromFore.user_password);
+
+                if (userFromDB != null)
+                {
+					var claims = new List<Claim>()
+    				{
+                        new Claim(ClaimTypes.Name, userFromDB.user_account),
+                        new Claim(ClaimTypes.GivenName,userFromDB.user_name),
+                        new Claim(ClaimTypes.NameIdentifier, userFromDB.user_id.ToString())
+                    };
+
+					//init the identity instances 
+					var userPrincipal = new ClaimsPrincipal(new ClaimsIdentity(claims, "user_cookie"));
+					await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal);
+					return RedirectToAction("Index", "Home");
+                } 
+                else
+    			{
+    				ViewBag.ErrMsg = "帐号和密码不正确";
+    				return View();
+    			}
+             }
         }
     }
 }
