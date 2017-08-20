bf426745d4b7fadb816efaf2882c83e8c76c06c7
LunaGao <gao.xue.qi@qq.com>
2017-08-20 15:55:11 +0800

    get remote IP address

---
+++ HideMessage_web/Models/Devices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HideMessage_web.Models
+{
+	public class Devices
+	{
+		[Key]
+		public int device_id { get; set; }
+		public string device_name { get; set; }
+		public string device_ip_v4 { get; set; }
+		public string device_code { get; set; }
+		public string device_sgin_password { get; set; }
+		public string device_client_id { get; set; }
+	}
+}
