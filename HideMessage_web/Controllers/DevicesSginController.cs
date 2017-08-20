fd64404b6c70bc14d8864f6995ce88c4d4fbeabb
LunaGao <gao.xue.qi@qq.com>
2017-08-20 15:55:11 +0800

    get remote IP address

--- HideMessage_web/Controllers/DevicesSginController.cs
+++ HideMessage_web/Controllers/DevicesSginController.cs
@@ -9,10 +9,18 @@ namespace HideMessage_web.Controllers
 {
 	public class DevicesSginController : Controller
 	{
-		public String Sgin(String SginCode, String password)
+        [HttpGet]
+		public String Sgin(String SginCode, String password, String cliendId)
 		{
-            string ip = Request.HttpContext.Connection.RemoteIpAddress.ToString();
-			return ip;
+            //string ipAddress2 = Request.HttpContext.Request.Headers["X-Real-IP"].FirstOrDefault();
+            string ipAddress = Request.Headers["X-Forwarded-For"].FirstOrDefault();
+
+            //string ipAddress = request.HttpContext.Connection.RemoteIpAddress;
+			//string ipAddress = this.Request.HttpContext.Connection.RemoteIpAddress.IsIPv4MappedToIPv6
+					//? Request.HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString()
+					//: Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            //string ip = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            return ipAddress + "(SginCode)" + SginCode + "(password)" + password;
 		}
 	}
 }
--- HideMessage_web/Models/User.cs
+++ HideMessage_web/Models/User.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
 namespace HideMessage_web.Models
 {
